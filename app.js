643ec789acc8e5acba70b87379089771817bb0fc
sivani-r-anil <sivaniranil28@gmail.com>
2026-07-15 15:14:46 +0530

    Delete and search is added

--- app.js
+++ app.js
@@ -1,21 +1,21 @@
-const express = require("express")
-const mongoose = require("mongoose")
-const cors = require("cors")
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
 
+const app = express();
+app.use(cors());
+app.use(express.json());
 
-
-const app = express()
-app.use(cors())
-app.use(express.json())
-
-mongoose.connect("mongodb://sree:sree123@ac-u3sw13b-shard-00-00.ckegugb.mongodb.net:27017,ac-u3sw13b-shard-00-01.ckegugb.mongodb.net:27017,ac-u3sw13b-shard-00-02.ckegugb.mongodb.net:27017/hosteldb?ssl=true&replicaSet=atlas-3xyrxx-shard-0&authSource=admin&appName=Cluster0").then(
-    () => {
-        console.log("MongoDb connected")
-    }
-).catch(
-    (error) => {
-        console.log(error)
+mongoose
+    .connect(
+        "mongodb://sree:sree123@ac-u3sw13b-shard-00-00.ckegugb.mongodb.net:27017,ac-u3sw13b-shard-00-01.ckegugb.mongodb.net:27017,ac-u3sw13b-shard-00-02.ckegugb.mongodb.net:27017/hosteldb?ssl=true&replicaSet=atlas-3xyrxx-shard-0&authSource=admin&appName=Cluster0",
+    )
+    .then(() => {
+        console.log("MongoDb connected");
     })
+    .catch((error) => {
+        console.log(error);
+    });
 
 // ----- Leave Schema ----- //
 const Leave = mongoose.model(
@@ -32,21 +32,26 @@ const Leave = mongoose.model(
         toDate: String,
         reasonForLeave: String,
         parentContactNumber: String,
-        leaveStatus: String
-    })
+        leaveStatus: String,
+    }),
 );
 
 app.post("/add-leave", async (req, res) => {
     await Leave.create(req.body);
     res.json({
-        Status: "Success"
+        Status: "Success",
     });
 });
 
+app.post("/view-leave", async (req, res) => {
+    const leaves = await Leave.find();
+    res.json(leaves);
+});
 
 //----- Add Students ------ //
-const student = mongoose.model("Students", new mongoose.Schema(
-    {
+const student = mongoose.model(
+    "Students",
+    new mongoose.Schema({
         admissionId: String,
         studentID: String,
         name: String,
@@ -58,25 +63,81 @@ const student = mongoose.model("Students", new mongoose.Schema(
         parentName: String,
         parentNo: String,
         date: String,
-        wardenName: String
-    }
-))
-
+        wardenName: String,
+    }),
+);
 
 app.post("/add-student", async (req, res) => {
-    await student.create(req.body)
-    res.json({ "status": "success" })
-})
+    await student.create(req.body);
+    res.json({ status: "success" });
+});
 
 app.post("/view-students", async (req, res) => {
-    const students = await student.find()
-    res.json(students)
-})
+    const students = await student.find();
+    res.json(students);
+});
 
+app.post("/delete-student", async (req, res) => {
+    try {
+        const { _id } = req.body;
+        await student.findByIdAndDelete(_id);
+        res.json({
+            status: "success",
+            message: "Deleted successfully",
+        });
+    } catch (error) {
+        res.status(500).json({
+            status: "error",
+            message: "Failed to delete student",
+        });
+    }
+});
 
+app.post("/update-student", async (req, res) => {
+    try {
+        const updatedStudent = req.body;
+        await student.findByIdAndUpdate(updatedStudent._id, updatedStudent);
+        res.json({
+            status: "success",
+            message: "Updated successfully",
+        });
+    } catch (error) {
+        res.status(500).json({
+            status: "error",
+            message: "Failed to update student",
+        });
+    }
+});
 
-const logout = mongoose.model("Logout", new mongoose.Schema(
-    {
+//-----LogIn-----//
+const HostelLogin = mongoose.model(
+    "logins",
+    new mongoose.Schema({
+        loginId: String,
+        studentId: String,
+        studentName: String,
+        hostelBlock: String,
+        roomNumber: String,
+        department: String,
+        yearOfStudy: String,
+        loginDate: String,
+        loginTime: String,
+        attendanceStatus: String,
+    }),
+);
+app.post("/login-add", async (req, res) => {
+    await HostelLogin.create(req.body);
+    res.json({ status: "success" });
+});
+app.get("/login-view", async (req, res) => {
+    const logins = await HostelLogin.find();
+    res.json(logins);
+});
+
+//-----LogOut-----//
+const logout = mongoose.model(
+    "Logout",
+    new mongoose.Schema({
         logoutId: String,
         studentID: String,
         name: String,
@@ -87,25 +148,20 @@ const logout = mongoose.model("Logout", new mongoose.Schema(
         logoutTime: String,
         logoutDate: String,
         purpose: String,
-        returnTime: String
-    }
-))
-
+        returnTime: String,
+    }),
+);
 
 app.post("/logout", async (req, res) => {
-    await logout.create(req.body)
-    res.json({ "status": "success" })
-})
+    await logout.create(req.body);
+    res.json({ status: "success" });
+});
 
 app.post("/view-logout", async (req, res) => {
-    const logouts = await logout.find()
-    res.json(logouts)
-})
+    const logouts = await logout.find();
+    res.json(logouts);
+});
 
 app.listen(3000, () => {
-    console.log("server started")
-}).catch(
-    (error) => {
-        console.log("error")
-    }
-)
+    console.log("Server Started");
+});
