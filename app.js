020182ef7a2a776a0eff8eddce23f10184a7569b
Bimigijoo <bimigijo1212@gmail.com>
2026-07-15 11:15:48 +0530

    Implemented Add Leave API

--- app.js
+++ app.js
@@ -14,6 +14,35 @@ mongoose.connect("mongodb://sree:sree123@ac-u3sw13b-shard-00-00.ckegugb.mongodb.
     (error)=>{
         console.log(error)
     })
-    app.listen(3000,()=>{
-        console.log("server started")
-    })
+
+// ----- Leave Schema ----- //
+const Leave = mongoose.model(
+  "leaves",
+  new mongoose.Schema({
+    leaveRequestId: String,
+    studentId: String,
+    studentName: String,
+    hostelBlock: String,
+    roomNumber: String,
+    department: String,
+    yearOfStudy: String,
+    fromDate: String,
+    toDate: String,
+    reasonForLeave: String,
+    parentContactNumber: String,
+    leaveStatus: String
+  })
+);
+
+app.post("/add-leave", async (req, res) => {
+    await Leave.create(req.body);
+    res.json({
+        Status: "Success"
+    });
+});
+
+
+
+app.listen(3000,()=>{
+    console.log("server started")
+})
